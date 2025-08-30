904ebb031e90d9047c996ad79ba788920f559fd7
Tomaz Boyd <boydt@sandhills.edu>
2025-08-30 15:22:16 -0400

    Done and finished square root

---
+++ squareroot.cpp
@@ -0,0 +1,21 @@
+//@author: Tomaz Boyd
+//@date: 08/27/2025
+//@purpose: This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+#include <cmath>
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    double num1 = 90;
+    double num2 = 0;
+    double SquareRoot = sqrt(num1);
+    //prints comment from 2 numbers above
+    std::cout << "The Squareroot of " << num1 << " is " << SquareRoot << std::endl;
+
+    return 0;
+}
