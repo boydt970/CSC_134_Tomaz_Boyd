12c706549041dcb2bd0f200907b0e8b93a975dea
Tomaz Boyd <boydt@sandhills.edu>
2025-08-30 15:10:48 -0400

    8/30/25 complete

---
+++ my_division_program.cpp
@@ -0,0 +1,21 @@
+//@author: Tomaz Boyd
+//@date: 08/27/2025
+//@purpose: This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    int num1 = 90;
+    int num2 = 0;
+    int quotient = num1 / num2;
+    //prints comment from 2 numbers above
+    std::cout << quotient << std::endl;
+
+    return 0;
+}
