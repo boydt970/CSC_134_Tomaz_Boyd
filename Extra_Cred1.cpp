3e8a3f7c250c6b12f4cb055a93a7eee7191d3868
Tomaz Boyd <boydt@sandhills.edu>
2025-08-31 12:54:58 -0400

    Hi! is printed

---
+++ Extra_Cred1.cpp
@@ -0,0 +1,21 @@
+//@author: Tomaz Boyd
+//@date: 08/31/2025
+//@purpose: This program prints Hi!
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+   char H, I, x;// store character variable
+   H = 'H';
+   I = 'i';
+   x = '!';
+   //prints Hi!
+   cout << H << I << x << endl;
+   
+    return 0;
+}
