080d7a703ab363c2b3cc1a072fd57779e83931a5
Tomaz Boyd <boydt@sandhills.edu>
2025-09-01 10:23:57 -0400

    used double for the float

---
+++ Extra_Cred3.cpp
@@ -0,0 +1,21 @@
+//@author: Tomaz Boyd
+//@date: 09/1/2025
+//@purpose: This program calculates the product of two numbers
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    double num1 = 5.5;
+    double num2 = 10.2;
+    double product = num1 + num2;
+    //prints comment from 2 numbers above
+    std::cout << product << std::endl;
+
+    return 0;
+}
