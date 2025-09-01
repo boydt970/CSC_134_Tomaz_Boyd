d8257251a3b5e8972631ae1cd4d54b85895887ab
Tomaz Boyd <boydt@sandhills.edu>
2025-09-01 10:34:17 -0400

    E notation created

---
+++ Extra_Cred4.cpp
@@ -0,0 +1,20 @@
+//@author: Tomaz Boyd
+//@date: 09/1/2025
+//@purpose: This program calculates the product of 2 floating-point values
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    float Avogadro;//float created
+    Avogadro = 6.02214076E23;//also can use Avogadro = 6.02214076f or 6.02214076L
+    //prints the stored number from Avogadro
+    std::cout << Avogadro << std::endl;
+
+    return 0;
+}
