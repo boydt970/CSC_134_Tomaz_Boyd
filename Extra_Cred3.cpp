dea1d23b11e5f885e78330041c11d99ae60031fb
Tomaz Boyd <boydt@sandhills.edu>
2025-09-02 14:54:06 -0400

    input comm

--- Extra_Cred3.cpp
+++ Extra_Cred3.cpp
@@ -1,6 +1,6 @@
 //@author: Tomaz Boyd
 //@date: 09/1/2025
-//@purpose: This program calculates the product of two numbers
+//@purpose: This program calculates the product of 2 floating-point values
 
 //include the iostream directive
 #include <iostream>
@@ -11,10 +11,10 @@ using namespace std;
 //define the main() function/method
 int main()
 {
-    double num1 = 5.5;
-    double num2 = 10.2;
-    double product = num1 + num2;
-    //prints comment from 2 numbers above
+    double num1 = 5.5;//5.5 vaule as a float
+    double num2 = 10.2;//10.2 value as a float
+    double product = num1 + num2;//the adding of both values
+    //prints results from 2 numbers above
     std::cout << product << std::endl;
 
     return 0;
