2c7fbc34066d04316aed06c150a56bd35af6d9be
Tomaz Boyd <boydt@sandhills.edu>
2025-09-03 17:01:23 -0400

    comments made

--- Lab #3/Extra_Cred6.cpp
+++ Lab #3/Extra_Cred6.cpp
@@ -1,6 +1,6 @@
 //@author: Tomaz Boyd
 //@date: 09/1/2025
-//@purpose: This program converts fahrenheit to celsius
+//@purpose: This program finds area of circle
 
 //include the iostream directive
 #include <iostream>
@@ -11,13 +11,13 @@ using namespace std;
 //define the main() function/method
 int main()
 {
-    double pi;
-    double radius;
-    double A;
-    pi = 3.1415;//empty varible to be assigned later
-    radius = 19 * 19;//fahrenheit degrees
-    A = pi * radius;//equation to determine C/Celsius
-    //printing temperatures in both forms
+    double pi;//
+    double radius;// radius of circle
+    double A;// area
+    pi = 3.1415;// pie measurement
+    radius = 19 * 19;//radius to the power of 2
+    A = pi * radius;//equation to determine Area
+    //printing Area of circle
     std::cout << "Area equals " << A << std::endl;
 
     return 0;
