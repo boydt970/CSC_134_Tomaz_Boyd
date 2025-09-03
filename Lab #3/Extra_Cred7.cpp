4fee49a25d081ae9fce6709fdeb3f5d1b0d9b41e
Tomaz Boyd <boydt@sandhills.edu>
2025-09-03 16:57:35 -0400

    volume of sphere

---
+++ Lab #3/Extra_Cred7.cpp
@@ -0,0 +1,24 @@
+//@author: Tomaz Boyd
+//@date: 09/1/2025
+//@purpose: This program converts fahrenheit to celsius
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    double V;
+    double pi;
+    double radius;
+    pi = 3.1415;//pi math measurement
+    radius = 7 * 7 * 7;//radius 7 to the 3rd power
+    V = 4.0/3.0 * (pi * radius);//equation to determine volume
+    //printing volume of a sphere
+    std::cout << "Volume equals " << V << std::endl;
+
+    return 0;
+}
