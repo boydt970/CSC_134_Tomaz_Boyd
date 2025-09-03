b46105fec80cf9b7666e246fd7dc1588cf847796
Tomaz Boyd <boydt@sandhills.edu>
2025-09-03 11:52:44 -0400

    pi

---
+++ Lab #3/Extra_Cred6.cpp
@@ -0,0 +1,24 @@
+//@author: Tomaz Boyd
+//@date: 09/1/2025
+//@purpose: This program converts fahrenheit to celsius
+
+//include the iostream directive
+#include <iostream>
+
+//use the standard namespace 
+using namespace std;
+
+//define the main() function/method
+int main()
+{
+    double pi;
+    double radius;
+    double A;
+    pi = 3.1415;//empty varible to be assigned later
+    radius = 19 * 19;//fahrenheit degrees
+    A = pi * radius;//equation to determine C/Celsius
+    //printing temperatures in both forms
+    std::cout << "Area equals " << A << std::endl;
+
+    return 0;
+}
