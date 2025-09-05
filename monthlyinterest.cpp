fb791c459c43b9e2028ac668d397c5ecabef045f
Tomaz Boyd <boydt@sandhills.edu>
2025-09-05 15:59:31 -0400

    interest monthly

--- monthlyinterest.cpp
+++ monthlyinterest.cpp
@@ -14,8 +14,9 @@ int main()
     double balance = 3284.87;// monthly balance
     double interest = .2899 / 12;//interest rate
     double nextdue = balance + (interest * balance);// calculation for interest add on
+    double interestincrease = interest * balance;
     //prints next months interest
-    std::cout << "Next months balance " << nextdue << std::endl;
-
+    std::cout << "Next months balance is " << nextdue << " dollars." << std::endl;
+    std::cout << "You paid a total of " << interestincrease << " dollars in interest for the month." << std::endl;
     return 0;
 }
