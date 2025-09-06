7f8374e1c76c014aaff0b5cabd9d0b57310bda54
Tomaz Boyd <boydt@sandhills.edu>
2025-09-06 08:54:05 -0400

    commit

--- monthlyinterest.cpp
+++ monthlyinterest.cpp
@@ -14,17 +14,17 @@ int main()
 
     double balance,interest,newinterest;
     cout << "what is your current balance: ";
-    cin >> balance;
-    //double balance = 3284.87;// monthly balance
+    cin >> balance;//user input
+
     cout << "what is your current interest rate: ";
-    cin >> interest;
-    //double interest = .2899 / 12;//interest rate
-    newinterest = (interest / 100) / 12;
+    cin >> interest;//user input
+    
+    newinterest = (interest / 100) / 12;//interest calculation program
     double nextdue = balance + (newinterest * balance);// calculation for interest add on
     double interestincrease = newinterest * balance;
     //prints next months interest
     std::cout << "Next months balance is " << nextdue << " dollars." << std::endl;
     std::cout << "You paid a total of " << interestincrease << " dollars in interest for the month." << std::endl;
-    
+
     return 0;
 }
