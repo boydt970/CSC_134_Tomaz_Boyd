271f3a9b61082ac0cbb1eadc08cdc0c32ec30c1a
Tomaz Boyd <boydt@sandhills.edu>
2025-09-06 08:50:15 -0400

    adds user input

--- monthlyinterest.cpp
+++ monthlyinterest.cpp
@@ -10,13 +10,21 @@ using namespace std;
 
 //define the main() function/method
 int main()
-{
-    double balance = 3284.87;// monthly balance
-    double interest = .2899 / 12;//interest rate
-    double nextdue = balance + (interest * balance);// calculation for interest add on
-    double interestincrease = interest * balance;
+{   
+
+    double balance,interest,newinterest;
+    cout << "what is your current balance: ";
+    cin >> balance;
+    //double balance = 3284.87;// monthly balance
+    cout << "what is your current interest rate: ";
+    cin >> interest;
+    //double interest = .2899 / 12;//interest rate
+    newinterest = (interest / 100) / 12;
+    double nextdue = balance + (newinterest * balance);// calculation for interest add on
+    double interestincrease = newinterest * balance;
     //prints next months interest
     std::cout << "Next months balance is " << nextdue << " dollars." << std::endl;
     std::cout << "You paid a total of " << interestincrease << " dollars in interest for the month." << std::endl;
+    
     return 0;
 }
