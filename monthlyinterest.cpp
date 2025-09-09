f83ab8f8aaf724360f2087ad3bcf5deb83616a35
Tomaz Boyd <boydt@sandhills.edu>
2025-09-08 23:34:24 -0400

    progress working

--- monthlyinterest.cpp
+++ monthlyinterest.cpp
@@ -13,14 +13,17 @@ int main()
 {   
 
     double balance,interest,newinterest;
-    double subtract = 400;
-    int count;
+    double subtract;
+    int count = 0;
     cout << "what is your current balance: ";
     cin >> balance;//user input
 
     cout << "what is your current interest rate: ";
     cin >> interest;//user input
     
+    cout << "payment you'd like to make monthly: ";
+    cin >> subtract;//user input
+
     newinterest = (interest / 100) / 12;//interest calculation program convert percentage to decimal
     double nextdue = balance + (newinterest * balance);// calculation for interest add on
     double interestincrease = newinterest * balance;
@@ -28,14 +31,18 @@ int main()
     //prints next months interest
     std::cout << "Next months balance is " << nextdue << " dollars." << std::endl;
     std::cout << "You paid a total of " << interestincrease << " dollars in interest for the month." << std::endl;
-    if (nextdue >= 0)
+    
+    while (nextdue > 0)
     {
-        double nextdue = balance + (newinterest * balance);// calculation for interest add on
-    double interestincrease = newinterest * balance;
-    nextdue = nextdue - subtract;
-    balance == nextdue;
-    cout << nextdue << endl;
+        ;// calculation for interest add on
+    interestincrease = newinterest * nextdue;
+    nextdue += interestincrease;
+    nextdue -= subtract;
+    count++;
+    
+    cout << count << ": " << nextdue << " you paid " << interestincrease << " in interest" << endl;
     }
+    cout << "At a paymemt of " << subtract << " dollars your balance will be finished in " << count << " months." << endl;
 
     //if (balance > 0)
     //while (balance >= 0) {
@@ -46,4 +53,4 @@ int main()
     //cout << balance << endl;
     //balance - 500; 
     return 0;
-}
+} 
